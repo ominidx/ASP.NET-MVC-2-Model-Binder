9796910c49de04a846c4fb504b31ec81c587e769
Naguesh <naguesh@hotmail.com>
2014-06-10 13:10:16 +0530

    Update ASPNETMVC2ModelBinder.cs

--- ASPNETMVC2ModelBinder.cs
+++ ASPNETMVC2ModelBinder.cs
@@ -1,30 +1,30 @@
 public class JSONDataModelBinder<T> : IModelBinder
+{
+    private string valueObjName { get; set; }
+
+    public JSONDataModelBinder(string objName) 
+    {
+        valueObjName = objName;
+    }
+    
+    public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+    {
+        // Get the raw attempted value from the value provider
+        string incomingData = bindingContext.ValueProvider.GetValue(valueObjName).AttemptedValue;
+
+        if (!string.IsNullOrEmpty(incomingData))
         {
-            private string valueObjName { get; set; }
-
-            public JSONDataModelBinder(string objName) 
-            {
-                valueObjName = objName;
-            }
+            // convert string to stream (2nd method)
+            //byte[] byteArray = Encoding.UTF8.GetBytes(incomingData);
+            //MemoryStream stream = new MemoryStream(byteArray);
+            //object o = new DataContractJsonSerializer(typeof(DailyLogSaveMobile)).ReadObject(stream);
             
-            public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
-            {
-                // Get the raw attempted value from the value provider
-                string incomingData = bindingContext.ValueProvider.GetValue(valueObjName).AttemptedValue;
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            var model = json_serializer.Deserialize<T>(incomingData);
 
-                if (!string.IsNullOrEmpty(incomingData))
-                {
-                    // convert string to stream (2nd method)
-                    //byte[] byteArray = Encoding.UTF8.GetBytes(incomingData);
-                    //MemoryStream stream = new MemoryStream(byteArray);
-                    //object o = new DataContractJsonSerializer(typeof(DailyLogSaveMobile)).ReadObject(stream);
-                    
-                    JavaScriptSerializer json_serializer = new JavaScriptSerializer();
-                    var model = json_serializer.Deserialize<T>(incomingData);
-
-                    return model;
-                }
-
-                return null;
-            }
+            return model;
         }
+
+        return null;
+    }
+}
